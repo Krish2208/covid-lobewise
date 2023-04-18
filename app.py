6b446e9e4cbb7c1efc15e81dd280c8285e978dc3
Krish Agrawal <32320437+Krish2208@users.noreply.github.com>
2023-04-18 10:58:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# @app.route('/about', methods=['GET'])
-# def about():
-#    return render_template('about.html')
+@app.route('/about', methods=['GET'])
+def about():
+   return render_template('about.html')
 
 
 def convertImgtoBase64(img):
